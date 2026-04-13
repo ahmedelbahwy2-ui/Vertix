25babfa25ee76ffe9da370e85bebccf350667aff
Loay-77 <mloay976@gmail.com>
2026-04-13 23:11:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+FILE_NAME = 'expenses.csv'
 
+def initialize_file():
+    try:
+        with open(FILE_NAME, 'x', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Date', 'Category', 'Description', 'Amount'])
+    except FileExistsError:
+        pass
 
 def add_expense():
     description = desc_entry.get()
